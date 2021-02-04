6df5cda258b6bd7418bf07dbde3f53e2cf88779e
Joey Whalen <jtktwhalen@gmail.com>
2021-02-03 22:54:06 -0500

    update Calculator to include add method

--- src/javaCalculatorTTSHW/Calculator.java
+++ src/javaCalculatorTTSHW/Calculator.java
@@ -1,15 +1,26 @@
 package javaCalculatorTTSHW;
 
 public class Calculator {
+	
+	// create a Java Calculator that adds, subtracts, multiplies, divides and gets the square of two integers.
 
-	public static void main(String[] args) {
-		// TODO create a Java Calculator Class that adds, subtracts, multiplies, divides and squares two integers.
-		private static final int NUM_1 = 4;
-		private static final int NUM_2 = 4;
-		
-		
-		
-
+	// add Method
+	public static int add(int num1, int num2) {
+		int add = 0;
+		add = num1 + num2;
+		return add;
 	}
-
+	
+	// subtract Method
+	
+	
+	public static void main(String[] args) {
+		int num1 = 4;
+		int num2 = 8;
+		
+		Calculator sum = new Calculator();
+		
+		int result = sum.add(num1, num2);
+		System.out.printf(" The sum is: " + result);
+	}
 }
