1753b4f226ce830f62a050c89d685cd4125a5b1c
Joey Whalen <jtktwhalen@gmail.com>
2021-02-03 23:11:56 -0500

    update Calculator to include subtract, multiply, and divide methods

--- src/javaCalculatorTTSHW/Calculator.java
+++ src/javaCalculatorTTSHW/Calculator.java
@@ -12,15 +12,43 @@ public class Calculator {
 	}
 	
 	// subtract Method
+	public static int subtract(int num1, int num2) {
+		int subtract = 0;
+		subtract = num1 - num2;
+		return subtract;
+	}
 	
+	// multiply Method
+	public static int multiply(int num1, int num2) {
+		int multiply = 0;
+		multiply = num1 * num2;
+		return multiply;
+	}
+	
+	// division Method
+	public static int divide(int num1, int num2) {
+		int divide = 0;
+		divide = num1 / num2;
+		return divide;
+	}
 	
 	public static void main(String[] args) {
-		int num1 = 4;
-		int num2 = 8;
+		int num1 = 8;
+		int num2 = 4;
 		
-		Calculator sum = new Calculator();
+		Calculator math = new Calculator();
 		
-		int result = sum.add(num1, num2);
-		System.out.printf(" The sum is: " + result);
+		int addResult = math.add(num1, num2);
+		System.out.println(" The sum is: " + addResult);
+		
+		int subtractResult = math.subtract(num1, num2);
+		System.out.println("The difference is " + subtractResult);
+		
+		int multiplyResult = math.multiply(num1, num2);
+		System.out.println("The product is " + multiplyResult);
+		
+		int divideResult = math.divide(num1, num2);
+		System.out.println("The quotient is " + divideResult);
+		
 	}
 }
