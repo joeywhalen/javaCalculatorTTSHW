019f6fb5b8d1eec4d2733943f913ed9e345a2c02
Joey Whalen <jtktwhalen@gmail.com>
2021-02-03 21:33:24 -0500

    update Calculator

--- src/javaCalculatorTTSHW/Calculator.java
+++ src/javaCalculatorTTSHW/Calculator.java
@@ -3,7 +3,12 @@ package javaCalculatorTTSHW;
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO create a Java Calculator Class that adds, subtracts, multiplies, divides and squares two integers.
+		private static final int NUM_1 = 4;
+		private static final int NUM_2 = 4;
+		
+		
+		
 
 	}
 
